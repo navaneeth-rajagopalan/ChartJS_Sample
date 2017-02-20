7c0a0ba2df850e4e9928f518fe93cece3948b9d1
navaneethnani <navaneethnvnthr@gmail.com>
2017-02-20 22:45:19 +0530

    Ajax calls added

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,38 +1,37 @@
-var chart = $("#chart");
-
-var myChart = new Chart(chart, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+$(document).ready(function(){
+    var chart = $("#chart");
+    var label = [];
+    var data = [];
+    $.ajax({
+        url: "http://localhost:5555/getSampleData/5000",
+        method: "GET",
+        dataType: "json",
+        success: function(data){
+            var myChart = new Chart(chart, {
+                type: 'horizontalBar',
+                data: {
+                    labels: data.labels,
+                    datasets: [{
+                        label: '# of incidents',
+                        data: data.data,
+                        borderWidth: 0
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    },
+                    stacked: false
                 }
-            }]
+            });
+        },
+        error: function(err){
+            console.log(err);
         }
-    }
+    })
+    
 });
--- server.js
+++ server.js
@@ -15,6 +15,17 @@ server.get("/", function(req, res){
     res.sendFile(__dirname+"/Public/index.html");
 });
 
+server.get("/getSampleData/:vol", function(req, res){
+    console.log("Req received..")
+    let dataVolume = req.params.vol,
+        sampleData = { labels: [], data: []};
+    for(let i = 1; i <= dataVolume; i++){
+        sampleData.labels.push(`Data ${i}`);
+        sampleData.data.push(parseInt(Math.random()*100));
+    }
+    res.send(200, sampleData);
+});
+
 server.listen(PORT, function(){
     console.log(`Listening on port ${PORT}`);
 })
